aa89e20612258a75accb378d536abaa7cb9af463
N8Grant <nathngrant@umass.edu>
2020-04-14 22:44:57 -0400

    redone website

---
+++ aboutmecaroucel.js
@@ -0,0 +1,3 @@
+$('.carousel-control-next').on('slide.bs.carousel', function (e) {
+    $('#myCarousel').carousel(e)    
+})
--- contactme.js
+++ contactme.js
@@ -1,29 +1,6 @@
-var nodemailer = require('nodemailer');
-
 function sendEmail() {
+  
   subj = document.getElementById('subject').value;
   mes = document.getElementById('message').value;
-
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'nathangrant456@gmail.com',
-      pass: 'Vacation2018'
-    }
-  });
-    
-  var mailOptions = {
-    from: 'nathangrant456@gmail.com',
-    to: 'nathangrant456@gmail.com',
-    subject: subj,
-    text: mes
-  };
-    
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  window.open('mailto:nathangrant456@gmail.com?subject='+subj+'&body='+mes);
 }
